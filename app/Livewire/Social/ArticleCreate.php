c450949fc70351ed644d878ed137f46104a90dbf
Vortechstudio <contact@vortechstudio.fr>
2024-02-24 19:37:24 +0100

    fix(app): Correction de la validation et de la création d'article
    - Correction de la validation des champs du formulaire
    - Ajout de la création d'un nouvel article avec les données saisies
    - Gestion des erreurs lors de la création de l'article
    - Ajout de la redirection vers la liste des articles après la création

--- app/Livewire/Social/ArticleCreate.php
+++ app/Livewire/Social/ArticleCreate.php
@@ -2,15 +2,25 @@
 
 namespace App\Livewire\Social;
 
+use App\Enums\Social\ArticleTypeEnum;
+use App\Models\Social\Article;
 use App\Models\Social\Cercle;
 use App\Models\User\User;
+use App\Services\Github\Issues;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\Lazy;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Monolog\Level;
+use Monolog\LogRecord;
+use Spatie\LaravelOptions\Options;
+use Vortechstudio\Helpers\Helpers;
 
 class ArticleCreate extends Component
 {
-    use LivewireAlert;
+    use LivewireAlert, WithFileUploads;
     public $cercles;
     public $authors;
     public $title = '';
@@ -23,17 +33,75 @@ class ArticleCreate extends Component
     public $promote = false;
     public $author = '';
     public $cercle_id = '';
+    public $type = '';
 
+    #[Validate('image|max:2048')]
+    public $image;
 
     public function mount()
     {
         $this->cercles = Cercle::all();
         $this->authors = User::where('admin', true)->get();
     }
+
+    public function updatedPublished($value)
+    {
+        $this->published = $value;
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            "title" => "required|alpha_num|min:5",
+            "description" => "max:255",
+            "contenue" => "required",
+            "cercle_id" => "required",
+            "author" => "required",
+            "type" => "required"
+        ]);
+
+        try {
+            Article::create([
+                "title" => $this->title,
+                "description" => $this->description,
+                "type" => $this->type,
+                "contenue" => $this->contenue,
+                "published" => $this->published,
+                "published_at" => $this->published_at ?? null,
+                "publish_social" => $this->publish_social,
+                "publish_social_at" => $this->publish_social_at ?? null,
+                "promote" => $this->promote,
+                "author" => $this->author,
+                "cercle_id" => $this->cercle_id
+            ]);
+        }catch (\Exception $exception) {
+            \Log::emergency($exception->getMessage(), [$exception]);
+            $this->alert("error", "Erreur lors de la création de l'article");
+            $issue = new Issues(new LogRecord(
+                new \DateTimeImmutable("now"),
+                "Article",
+                Level::Error,
+                $exception->getMessage(),
+                $exception->getTrace(),
+                [
+                    "code" => $exception->getCode(),
+                    "line" => $exception->getLine(),
+                    "file" => $exception->getFile()
+                ]
+            ));
+            $issue->createIssueFromException(true);
+        }
+
+        $this->alert("success", "L'article a été créé");
+        $this->redirectRoute('social.articles.index');
+    }
+
     #[Title("Création d'un article")]
     public function render()
     {
-        return view('livewire.social.article-create')
+        return view('livewire.social.article-create', [
+            "types" => Options::forEnum(ArticleTypeEnum::class)->toArray()
+        ])
             ->layout("layouts.app");
     }
 }
--- resources/views/livewire/social/article-create.blade.php
+++ resources/views/livewire/social/article-create.blade.php
@@ -15,7 +15,7 @@
                             required="true" />
                         <x-form.textarea
                             name="description"
-                            label="description" />
+                            label="Courte description" />
 
                         <x-form.textarea
                             type="tinymce"
@@ -29,11 +29,87 @@
                 <div class="card shadow-sm">
                     <div class="card-body">
                         <div class="mb-10">
-                            <label for="{{ $name }}" class="form-label {{ $required ? 'required' : '' }}">{{ $label }}</label>
+                            <label for="cercle_id" class="form-label required">Cercle</label>
+                            <div wire:ignore>
+                                <select name="cercle_id" class="form-select" data-control="select2" data-placeholder="-- Selectionner un cercle --" data-pharaonic="select2"  wire:model="cercle_id">
+                                    <option value=""></option>
+                                    @foreach($cercles as $cercle)
+                                        <option value="{{ $cercle->id }}">{{ $cercle->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mb-10">
+                            <label for="type" class="form-label required">Type</label>
+                            <div wire:ignore>
+                                <select name="type" class="form-select" data-control="select2" data-placeholder="-- Selectionner un type d'article --" data-pharaonic="select2"  wire:model="type">
+                                    <option value=""></option>
+                                    @foreach($types as $item)
+                                        <option value="{{ $item['value'] }}">{{ $item['label'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mb-10">
+                            <label for="type" class="form-label required">Auteur</label>
+                            <div wire:ignore>
+                                <select name="author" class="form-select" data-control="select2" data-placeholder="-- Selectionner un auteur --" data-pharaonic="select2"  wire:model="author">
+                                    <option value=""></option>
+                                    @foreach($authors as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="separator border border-2 border-gray-300 my-5"></div>
+                        <div class="d-flex flex-row justify-content-between align-items-center mb-10">
+                            <x-form.checkbox
+                                name="promote"
+                                label="Promouvoir l'article" />
+                        </div>
+                        <div x-data="{show_published_at: false}" class="d-flex flex-row justify-content-between align-items-center mb-10">
+                            <x-form.switches
+                                name="published"
+                                label="Publié l'article"
+                                value="1"
+                                class-check="primary"
+                                alpine="true"
+                                fun-alpine="show_published_at = ! show_published_at" />
+
+                            <div x-show="show_published_at">
+                                <label for="published_at" class="form-label">Publié le:</label>
+                                <div data-format="datetime" class="input-group" id="kt_td_picker_simple" data-td-target-input="nearest" data-td-target-toggle="nearest">
+                                    <input id="kt_td_picker_basic_input" wire:model="published_at" type="text" class="form-control" data-td-target="#published_at"/>
+                                    <span class="input-group-text" data-td-target="#published_at" data-td-toggle="datetimepicker">
+                                        <i class="ki-duotone ki-calendar fs-2"><span class="path1"></span><span class="path2"></span></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div x-data="{show_published_social_at: false}" class="d-flex flex-row justify-content-between align-items-center mb-10">
+                            <x-form.switches
+                                name="publish_social"
+                                label="Publié l'article sur les réseaux"
+                                value="1"
+                                class-check="warning"
+                                alpine="true"
+                                fun-alpine="show_published_social_at = ! show_published_social_at" />
+
+                            <div x-show="show_published_social_at">
+                                <label for="publish_social_at" class="form-label">Publié le:</label>
+                                <div data-format="datetime" class="input-group" id="kt_td_picker_simple" data-td-target-input="nearest" data-td-target-toggle="nearest">
+                                    <input id="kt_td_picker_basic_input" wire:model="publish_social_at" type="text" class="form-control" data-td-target="#publish_social_at"/>
+                                    <span class="input-group-text" data-td-target="#publish_social_at" data-td-toggle="datetimepicker">
+                                        <i class="ki-duotone ki-calendar fs-2"><span class="path1"></span><span class="path2"></span></i>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="card-footer">
-                        Footer
+                        <x-form.button
+                            text-submit="Créer un article"
+                            text-loading="Création en cours..." />
                     </div>
                 </div>
             </div>
