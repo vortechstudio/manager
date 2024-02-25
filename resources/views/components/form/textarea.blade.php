5adafda564a8abf81c81e0a026bdb466fe2b7795
Vortechstudio <contact@vortechstudio.fr>
2024-02-25 16:27:51 +0100

    fix(frontend): Correction de la barre d'outils TinyMCE
    Correction de la configuration de la barre d'outils TinyMCE pour supprimer les options non prises en charge. La barre d'outils affiche maintenant les options correctes pour l'édition de texte.
    
    Référence: #21

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -71,9 +71,9 @@
             let options = {
                 selector: "#{{ $name }}",
                 height: "480",
-                plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visulablocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc inserdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+                plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor advlist lists wordcount textpattern noneditable help charmap quickbars',
                 menubar: 'file edit view insert format tools table help',
-                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media template link anchor codesample',
+                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | forecolor backcolor removeformat | pagebreak | charmap | fullscreen preview save print | insertfile image media template link anchor codesample',
                 language: 'fr_FR',
                 setup: function (editor) {
                     editor.on('init change', function () {
