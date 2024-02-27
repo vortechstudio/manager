98606a8b46721550972ac310e1a0c9187dc27d57
maxim <contact@vortechstudio.fr>
2024-02-27 19:39:23 +0100

    fix(Social): Correction du titre de la vue article-edit
    - Ajout de l'attribut Title à la classe ArticleEdit
    - Ajout du layout app à la vue article-edit

--- app/Livewire/Social/ArticleEdit.php
+++ app/Livewire/Social/ArticleEdit.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Social;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class ArticleEdit extends Component
 {
+    #[Title("Edition d'un article")]
     public function render()
     {
-        return view('livewire.social.article-edit');
+        return view('livewire.social.article-edit')
+            ->layout('layouts.app');
     }
 }
