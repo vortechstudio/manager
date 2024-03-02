4fff5e2706728d75d73bdbe1ea13c7e004243783
Vortechstudio <contact@vortechstudio.fr>
2024-03-02 22:28:59 +0100

    fix(layout): Correction de l'icône favicon dans le fichier de mise en page
    Correction du chemin de l'icône favicon dans le fichier de mise en page pour qu'il pointe correctement vers le répertoire racine.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <!--begin::Head-->
 <head><base href=""/>
-    <title>{{ $title ?? 'Page Title' }} - {{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="/media/logos/favicon.ico" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
@@ -13,6 +13,7 @@
     <link href="{{ asset('/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" type="text/css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css'])
     @yield("styles")
     @stack("styles")
