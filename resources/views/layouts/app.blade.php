44b30e7590d93d72847a89121ad5db3d10a36da6
Vortechstudio <contact@vortechstudio.fr>
2024-03-11 17:01:07 +0100

    fix(UI): Corriger le chargement différé des polices Google
    Corriger le chargement différé des polices Google pour améliorer les performances de chargement de la page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="/media/logos/favicon.ico" />
     <!--begin::Fonts(mandatory for all pages)-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <link rel="stylesheet" defer href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
-    <link href="{{ asset('/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link defer href="{{ asset('/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link defer href="{{ asset('/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     @livewireStyles
     @vite(['resources/css/app.css'])
     @yield("styles")
