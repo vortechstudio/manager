321a99a0ea0d3f5b744ec838600bd99be82f4c62
Vortechstudio <contact@vortechstudio.fr>
2024-03-15 18:54:45 +0100

    fix(Middleware): Utiliser response au lieu de abort pour 403
    - Remplacer abort par response dans IsAdminMiddleware
    - Modifier la version PHP dans qodana.yaml à "8.2"
    - Ajouter des inspections PHP et exclure certains fichiers
    - Supprimer les méthodes FortifyServiceProvider inutilisées

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,11 +28,6 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
         RateLimiter::for('login', function (Request $request) {
             $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
 
