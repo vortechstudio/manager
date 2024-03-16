d42898e5bac2cddb906e40a9939e3bf1a78bf248
Vortechstudio <contact@vortechstudio.fr>
2024-03-16 16:34:58 +0100

    fix(UI): Correction de l'affichage de la page show.blade.php
    Ajout d'une card pour encadrer le contenu de la page et ajout d'un bouton pour éditer la page.

--- resources/views/social/pages/show.blade.php
+++ resources/views/social/pages/show.blade.php
@@ -10,7 +10,20 @@
         return="true"
         sticky="true" />
 
-    
+    <div class="card shadow-sm">
+        <div class="card-header">
+            <h3 class="card-title">&nbsp;</h3>
+            <div class="card-toolbar">
+                <a href="{{ route('social.pages.edit', $page->id) }}" class="btn btn-sm btn-light">
+                    Editer la page
+                </a>
+            </div>
+        </div>
+        <div class="card-body">
+            {!! $page->translateOrNew('fr')->content !!}
+        </div>
+    </div>
+
 @endsection
 
 @push("scripts")
