2a6a33e3b3eed310d197783efa84c5a3240d56ee
Vortechstudio <contact@vortechstudio.fr>
2024-04-01 13:29:46 +0200

    refactor(models): mise à jour des relations entre les modèles Event et Cercle
    - Mise à jour des relations dans les modèles Event et Cercle pour correspondre aux nouvelles tables pivot
    - Ajout des attributs 'type_label' et 'status_label' dans le modèle Event
    - Ajout de la méthode statique getImage dans le modèle Event pour récupérer les images
    - Correction des erreurs de syntaxe et de logique dans les modèles

--- app/Models/Social/Event.php
+++ app/Models/Social/Event.php
@@ -17,15 +17,20 @@ class Event extends Model
     public $timestamps = false;
 
     protected $casts = [
-        'start_at' => 'timestamp',
-        'end_at' => 'timestamp',
+        'start_at' => 'datetime',
+        'end_at' => 'datetime',
         'status' => EventStatusEnum::class,
         'type_event' => EventTypeEnum::class,
     ];
 
+    protected $appends = [
+        'type_label',
+        'status_label',
+    ];
+
     public function cercles()
     {
-        return $this->belongsToMany(Cercle::class);
+        return $this->belongsToMany(Cercle::class, 'event_cercle', 'event_id', 'cercle_id');
     }
 
     public function participants()
@@ -36,5 +41,39 @@ class Event extends Model
     public function poll()
     {
         return $this->hasOne(Poll::class);
+    }
+
+    public function getTypeLabelAttribute()
+    {
+        return match ($this->type_event) {
+            EventTypeEnum::POLL => '<span class="badge badge-primary"><i class="fa-solid fa-poll text-white me-2"></i> Sondage</span>',
+            EventTypeEnum::GRAPHIC => '<span class="badge badge-warning"><i class="fa-solid fa-image text-white me-2"></i> Graphique</span>',
+        };
+    }
+
+    public function getStatusLabelAttribute()
+    {
+        return match ($this->status) {
+            EventStatusEnum::DRAFT => '<span class="badge badge-secondary"><i class="fa-solid fa-pencil text-white me-2"></i> Brouillon</span>',
+            EventStatusEnum::PROGRESS => '<span class="badge badge-success"><i class="fa-solid fa-exchange-alt text-white me-2"></i> En cours...</span>',
+            EventStatusEnum::SUBMITTING => '<span class="badge badge-primary"><i class="fa-solid fa-envelope text-white me-2"></i> Soumission en cours...</span>',
+            EventStatusEnum::EVALUATION => '<span class="badge badge-info"><i class="fa-solid fa-envelope text-white me-2"></i> Evaluation en cours...</span>',
+            EventStatusEnum::CLOSED => '<span class="badge badge-danger"><i class="fa-solid fa-check-circle text-white me-2"></i> Terminer</span>',
+        };
+    }
+
+    public static function getImage(int $event_id, string $type): string
+    {
+        $type = match ($type) {
+            'icon' => 'icon',
+            'header' => 'header',
+            'default' => 'default',
+        };
+
+        if (\Storage::exists('events/'.$event_id.'/'.$type.'.webp')) {
+            return \Storage::url('events/'.$event_id.'/'.$type.'.webp');
+        } else {
+            return \Storage::url('events/'.$type.'_default.png');
+        }
     }
 }
