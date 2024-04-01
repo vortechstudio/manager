404db9de1e458aa25933c17ff19d0fdefbecf376
Vortechstudio <contact@vortechstudio.fr>
2024-04-01 17:13:56 +0200

    core(base): Correction de la taille de la ligne de soulignement
    Correction de la taille de la ligne de soulignement dans le composant Underline pour qu'elle soit dynamique en fonction de la propriété sizeLine spécifiée.

--- resources/views/components/base/underline.blade.php
+++ resources/views/components/base/underline.blade.php
@@ -3,6 +3,6 @@
         <span class="d-inline-block mb-2 {{ $styleText ?? 'fs-2tx fw-bold' }}">
             {{ $title ?? 'Titre' }}
         </span>
-        <span class="d-inline-block position-absolute h-8px bottom-0 end-0 start-0 bg-{{ $color ?? 'primary' }} translate rounded"></span>
+        <span class="d-inline-block position-absolute h-{{ $sizeLine }}px bottom-0 end-0 start-0 bg-{{ $color ?? 'primary' }} translate rounded"></span>
     </span>
 </div>
