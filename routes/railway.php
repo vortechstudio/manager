2fc5d9007f9d807375ca2858eb8618f4adc4ab3b
Vortechstudio <contact@vortechstudio.fr>
2024-04-07 15:40:19 +0200

    fix(route): Ajouter la route d'édition pour le matériel ferroviaire
    Ajoute la route pour l'édition du matériel ferroviaire dans le fichier railway.php.

--- routes/railway.php
+++ routes/railway.php
@@ -8,5 +8,7 @@ Route::prefix('railway')->as('railway.')->group(function () {
         Route::get('create', [\App\Http\Controllers\Railway\MaterielController::class, 'create'])->name('create');
         Route::post('create', [\App\Http\Controllers\Railway\MaterielController::class, 'store'])->name('store');
         Route::get('{engine}', [\App\Http\Controllers\Railway\MaterielController::class, 'show'])->name('show');
+        Route::get('{engine}/edit', [\App\Http\Controllers\Railway\MaterielController::class, 'edit'])->name('edit');
+        Route::put('{engine}/edit', [\App\Http\Controllers\Railway\MaterielController::class, 'update'])->name('update');
     });
 });
