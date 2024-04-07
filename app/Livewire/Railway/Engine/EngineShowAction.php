b36b0c59c23ee1f5bb03e20ecb58bc36840b342e
Vortechstudio <contact@vortechstudio.fr>
2024-04-07 16:35:11 +0200

    feat(Railway): Ajout de fonctionnalités pour désactiver, activer, mettre en production et supprimer un moteur de train
    - La fonction disabled permet de désactiver un moteur de train en le mettant inactif
    - La fonction enabled permet d'activer un moteur de train en le mettant actif
    - La fonction production permet de mettre un moteur de train en production
    - La fonction delete permet de supprimer un moteur de train en fonction de son type
    
    Les alertes sont affichées pour confirmer chaque action effectuée.

--- app/Livewire/Railway/Engine/EngineShowAction.php
+++ app/Livewire/Railway/Engine/EngineShowAction.php
@@ -2,11 +2,55 @@
 
 namespace App\Livewire\Railway\Engine;
 
+use App\Models\Railway\Engine\RailwayEngine;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class EngineShowAction extends Component
 {
-    public $engine;
+    use LivewireAlert;
+
+    public RailwayEngine $engine;
+
+    public function disabled()
+    {
+        $this->engine->active = false;
+        $this->engine->save();
+
+        $this->alert('success', 'Le matériel est maintenant inactif');
+    }
+
+    public function enabled()
+    {
+        $this->engine->active = true;
+        $this->engine->save();
+
+        $this->alert('success', 'Le matériel est maintenant actif');
+    }
+
+    public function production()
+    {
+        $this->engine->visual = 'production';
+        $this->engine->save();
+
+        $this->alert('success', 'Le matériel est maintenant en production');
+    }
+
+    public function delete()
+    {
+        if ($this->engine->type_train->value == 'automotrice') {
+            for ($i = 0; $i <= $this->engine->technical->nb_wagon; $i++) {
+                \Storage::delete('engines/automotrice/' . $this->engine->slug . '-' . $i . '.gif');
+            }
+        } else {
+            \Storage::delete('engines/' . $this->engine->type_train->value . '/' . $this->engine->slug . '.gif');
+        }
+
+        $this->engine->delete();
+
+        $this->alert('success', 'Le matériel est maintenant supprimé');
+    }
+
     public function render()
     {
         return view('livewire.railway.engine.engine-show-action');
