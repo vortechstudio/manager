8c18239a82a38ffb25f597576579225e7b67a971
Vortechstudio <contact@vortechstudio.fr>
2024-04-08 21:32:37 +0200

    feat(Railway): Ajout de la fonctionnalité de pagination et de recherche dans la table des gares
    - Ajout de la pagination et de la recherche dans la table des gares
    - Mise en place de la fonctionnalité de tri par champ dans la table
    - Affichage des informations des gares avec la pagination et la recherche
    - Ajout des boutons pour gérer et supprimer une gare dans la table

--- app/Livewire/Railway/Hubs/HubTable.php
+++ app/Livewire/Railway/Hubs/HubTable.php
@@ -2,12 +2,51 @@
 
 namespace App\Livewire\Railway\Hubs;
 
+use App\Models\Railway\Gare\RailwayGare;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class HubTable extends Component
 {
+    use LivewireAlert, WithPagination;
+
+    public string $search = '';
+
+    public string $orderField = 'name';
+
+    public string $orderDirection = 'ASC';
+
+    public int $perPage = 10;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'orderField' => ['except' => 'name'],
+        'orderDirection' => ['except' => 'ASC'],
+    ];
+
+    public function setOrderField(string $name): void
+    {
+        if ($name === $this->orderField) {
+            $this->orderDirection = $this->orderDirection === 'ASC' ? 'DESC' : 'ASC';
+        } else {
+            $this->orderField = $name;
+            $this->reset('orderDirection');
+        }
+    }
+
+    public function paginationView()
+    {
+        return 'livewire.pagination';
+    }
+
     public function render()
     {
-        return view('livewire.railway.hubs.hub-table');
+        return view('livewire.railway.hubs.hub-table', [
+            'gares' => RailwayGare::with('weather', 'hub')
+            ->where('name', 'like', '%' . $this->search . '%')
+            ->orderBy($this->orderField, $this->orderDirection)
+            ->paginate($this->perPage),
+        ]);
     }
 }
--- resources/views/livewire/railway/hubs/hub-table.blade.php
+++ resources/views/livewire/railway/hubs/hub-table.blade.php
@@ -1,3 +1,95 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="d-flex flex-row align-items-end mb-5">
+        <div class="position-relative w-250px me-3">
+            <i class="ki-duotone ki-magnifier fs-2 text-gray-500 position-absolute top-50 translate-middle-y ms-4"><span class="path1"></span><span class="path2"></span></i>
+            <input type="text" class="form-control border-gray-200 h-40px bg-body ps-13 fs-7" wire:model.live.debounce.500ms="search" placeholder="Rechercher un évènement..." data-kt-search-element="input">
+        </div>
+        <select wire:model="perPage" class="form-select border-gray-200 h-40px bg-body ps-13 fs-7 w-100px me-5" id="perPage">
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+        </select>
+        <a href="{{ route('railway.hubs.create') }}" class="btn btn-outline btn-outline-primary"><i class="fa-solid fa-plus-circle me-3"></i> Création d'une gare</a>
+    </div>
+    <div class="table-responsive" wire:loading.class="opacity-50 bg-grey-700 table-loading">
+        <div class="table-loading-message">
+            <span class="spinner-border spinner-border-sm align-middle me-2"></span> Chargement...
+        </div>
+        <table class="table table-row-bordered table-row-gray-300 shadow-lg bg-info text-light rounded-4 table-striped gap-5 gs-5 gy-5 gx-5 align-middle">
+            <thead>
+            <tr class="fw-bold fs-3">
+                <x-base.table-header :direction="$orderDirection" name="id" :field="$orderField">#</x-base.table-header>
+                <x-base.table-header :direction="$orderDirection" name="name" :field="$orderField">Gare</x-base.table-header>
+                <x-base.table-header :direction="$orderDirection" name="type" :field="$orderField">Type</x-base.table-header>
+                <x-base.table-header :direction="$orderDirection" name="is_hub" :field="$orderField">Hub</x-base.table-header>
+                <th>Coordonnées</th>
+                <th>Nombre de Ligne</th>
+                <th></th>
+            </tr>
+            </thead>
+            @if($gares->count() > 0)
+                <tbody>
+                @foreach($gares as $gare)
+                    <tr>
+                        <td>{{ $gare->id }}</td>
+                        <td>{{ $gare->name }}</td>
+                        <td>{{ $gare->type_gare_string }}</td>
+                        <td>
+                            <div class="d-flex flex-column">
+                                <span class="mb-1">{!! $gare->formatIsHub() !!}</span>
+                                @if($gare->hub)
+                                    {!! $gare->hub->is_active_label !!}
+                                @endif
+                            </div>
+                        </td>
+                        <td>
+                            <div class="d-flex flex-column">
+                                <div class="d-flex flex-row align-items-center mb-1">
+                                    <i class="fa-solid fa-map-marked me-2"></i>
+                                    <span>{{ $gare->latitude }},</span>
+                                    <span>{{ $gare->longitude }}</span>
+                                </div>
+                                <div class="d-flex flex-row align-items-center mb-1">
+                                    <i class="fa-solid fa-house me-2"></i>
+                                    <span>{{ $gare->city }}</span>
+                                </div>
+                                <div class="d-flex flex-row align-items-center mb-1">
+                                    <i class="fa-solid fa-globe me-2"></i>
+                                    <span>{{ $gare->pays }}</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            @if($gare->is_hub)
+                                <span class="badge badge-primary">{{ $gare->hub->lignes()->count() }}</span>
+                            @endif
+                        </td>
+                        <td>
+                            <div class="btn-group btn-group-sm" role="group">
+                                <a href="{{ route('railway.hubs.show', $gare) }}" class="btn btn-icon btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Gérer la Gare">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <button wire:click="destroy({{ $gare->id }})" onclick="confirm('Voulez-vous supprimer cette gare ?') || event.stopImmediatePropagation()" class="btn btn-icon btn-danger" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Supprimer la gare">
+                                    <i class="fa-solid fa-trash" wire:loading.remove wire:target="destroy({{ $gare->id }})"></i>
+                                    <i class="fa-solid fa-spinner fa-spin-pulse" wire:loading wire:target="destroy({{ $gare->id }})"></i>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            @else
+                <tbody>
+                <tr>
+                    <td colspan="7">
+                        <x-base.is-null
+                            text="Aucunes gares enregistrées" />
+                    </td>
+                </tr>
+                </tbody>
+            @endif
+        </table>
+    </div>
+    {{ $gares->links() }}
 </div>
