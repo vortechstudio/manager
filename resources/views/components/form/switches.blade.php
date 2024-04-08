b38da05c883c4ea05a4d9bb07929adb46efc355d
Vortechstudio <contact@vortechstudio.fr>
2024-04-08 22:08:58 +0200

    feat(Railway): Ajout de la création d'une gare avec gestion des hubs
    Ajout de la méthode create() dans HubController pour afficher le formulaire de création d'une gare.
    Ajout de la méthode store() dans HubController pour enregistrer les informations de la gare.
    Création du template create.blade.php pour afficher le formulaire de création d'une gare.
    Ajout de la validation des données et de l'enregistrement de la gare en base de données.
    Ajout de la gestion des hubs avec la possibilité de définir si la gare est un hub.
    Ajout de la récupération des informations météorologiques pour la gare créée.

--- resources/views/components/form/switches.blade.php
+++ resources/views/components/form/switches.blade.php
@@ -1,4 +1,4 @@
-<div class="form-check form-switch form-check-custom {{ $classCheck ? 'form-check-'.$classCheck : '' }} form-check-solid" @if($alpine) x-on:change="{{ $funAlpine }}" @endif>
+<div class="form-check form-switch form-check-custom {{ $classCheck ? 'form-check-'.$classCheck : '' }} form-check-solid mb-2" @if($alpine) x-on:change="{{ $funAlpine }}" @endif>
     <input name="{{ $name }}" class="form-check-input {{ $size ? 'w-'.$size[0].'px h-'.$size[1].'px' : '' }}" type="checkbox" wire:model="{{ $name }}" @if($checked) checked="checked" @endif value="{{ $value }}" id="{{ $name }}"/>
     <label class="form-check-label" for="{{ $name }}">
         {{ $label }}
