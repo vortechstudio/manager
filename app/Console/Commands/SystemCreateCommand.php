3fe7088f6ae4ddbe885f4ebad085979321ee8733
Vortechstudio <contact@vortechstudio.fr>
2024-04-13 17:02:21 +0200

    fix(Railway): Modification de la génération des niveaux - Crée un nouveau fichier LevelAction.php dans le dossier app/Actions/Railway - Ajoute une méthode handle pour générer les récompenses et les niveaux - Supprime les anciens niveaux et récompenses avant de créer les nouveaux - Utilise les constantes de LevelRewardTypeEnum pour définir les types de récompenses - Utilise les valeurs aléatoires pour définir les montants des récompenses - Met à jour la méthode generateLevel dans SystemCreateCommand.php pour utiliser la nouvelle classe LevelAction

--- app/Console/Commands/SystemCreateCommand.php
+++ app/Console/Commands/SystemCreateCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Actions\Railway\EngineAction;
 use App\Actions\Railway\GareAction;
+use App\Actions\Railway\LevelAction;
 use App\Enums\Railway\Config\LevelRewardTypeEnum;
 use App\Models\Railway\Config\RailwayLevel;
 use App\Models\Railway\Config\RailwayLevelReward;
@@ -363,9 +364,8 @@ class SystemCreateCommand extends Command
         return [$gare->hub->id => $gare->name];
     }
 
-    private function generateLevel()
+    private function generateLevel(): void
     {
-        $this->generateRewards();
         $levelMax = text(
             label: 'Nombre de niveau maximum ?',
         );
@@ -374,91 +374,6 @@ class SystemCreateCommand extends Command
             label: 'XP de depart ?',
         );
 
-        info('Réinitialisation des niveaux');
-
-        foreach (RailwayLevel::all() as $level) {
-            $level->delete();
-        }
-
-        info('Création des niveaux');
-
-        for ($i = 0; $i <= $levelMax; $i++) {
-            RailwayLevel::create([
-                'id' => $i,
-                'exp_required' => $xpStart * ($i + config('railway.coef_level')),
-                'railway_level_reward_id' => RailwayLevelReward::all()->random()->id,
-            ]);
-        }
-    }
-
-    private function generateRewards()
-    {
-        $bases = collect();
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::ARGENT->name),
-            'value' => intval(round(rand(1000, 9999), -2, PHP_ROUND_HALF_UP)),
-
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::AUDIT_EXT->name),
-            'value' => intval(round(rand(2, 10), 0, PHP_ROUND_HALF_UP)),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::AUDIT_INT->name),
-            'value' => intval(round(rand(2, 10), 0, PHP_ROUND_HALF_UP)),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::ENGINE->name),
-            'value' => 0,
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::ENGINE_R->name),
-            'value' => 0,
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::IMPOT->name),
-            'value' => intval(round(rand(1000, 10000), -2, PHP_ROUND_HALF_UP)),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::RD_COAST->name),
-            'value' => intval(round(rand(1000, 10000), -2, PHP_ROUND_HALF_UP)),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::RD_RATE->name),
-            'value' => round(random_float(0, 0.5), 2, PHP_ROUND_HALF_ODD),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::SIMULATION->name),
-            'value' => intval(round(rand(2, 10), 0, PHP_ROUND_HALF_UP)),
-        ]);
-
-        $bases->push([
-            'type' => \Str::lower(LevelRewardTypeEnum::TPOINT->name),
-            'value' => intval(round(rand(5, 20), 2, PHP_ROUND_HALF_UP)),
-        ]);
-
-        foreach (RailwayLevelReward::all() as $reward) {
-            $reward->delete();
-        }
-
-        \DB::statement('ALTER TABLE `railway_level_rewards` AUTO_INCREMENT = 0;');
-
-        foreach ($bases as $reward) {
-            RailwayLevelReward::create([
-                'name' => \Str::ucfirst($reward['type']),
-                'type' => LevelRewardTypeEnum::tryFrom(\Str::lower($reward['type']))->value,
-                'action' => 'reward_'.$reward['type'],
-                'action_count' => $reward['value'],
-            ]);
-        }
+        (new LevelAction)->handle($levelMax, $xpStart);
     }
 }
