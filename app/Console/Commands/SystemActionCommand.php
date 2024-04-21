3df650515dfe249f6858f7fbbc0235e56cd713fd
Vortechstudio <contact@vortechstudio.fr>
2024-04-21 17:27:44 +0200

    feat(Scheduled Actions): Add a scheduled task to execute the 'dayly_flux' system action
    * Ajout d'une tâche programmée pour exécuter l'action système 'daily_flux'.

---
+++ app/Console/Commands/SystemActionCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Railway\Config\RailwayBanque;
+use App\Models\User\User;
+use App\Notifications\Users\SendMessageNotification;
+use Illuminate\Console\Command;
+
+class SystemActionCommand extends Command
+{
+    protected $signature = 'action {action}';
+
+    protected $description = 'Effectue des actions propre aux services';
+
+    public function handle(): void
+    {
+        match ($this->argument('action')) {
+            'dayly_flux' => $this->dailyFlux(),
+        };
+    }
+
+    private function dailyFlux()
+    {
+        foreach (RailwayBanque::all() as $bank) {
+            $bank->generate();
+        }
+
+        foreach (User::where('admin', true)->get() as $user) {
+            $user->notify(new SendMessageNotification(
+                'Flux Bancaire quotidien',
+                'Le flux des intêret bancaire à été mise à jours',
+                'info',
+                'fa-info-circle'
+            ));
+        }
+    }
+}
