9d8d38582deec33f8d70b604b1a5f25122fa7437
Vortechstudio <contact@vortechstudio.fr>
2024-04-21 20:39:53 +0200

    core(Dev): Génération d'information de test

---
+++ database/seeders/Test/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders\Test;
+
+use App\Models\User\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Administrateur',
+            'email' => 'admin@admin.com',
+            'admin' => true,
+        ]);
+
+        User::factory(10)->create();
+    }
+}
