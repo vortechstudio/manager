ff8c7a668e21bccb413bfa6c5fbb7dab5eba8318
Vortechstudio <contact@vortechstudio.fr>
2024-04-25 21:46:13 +0200

    feat(Railway Banque): Mise en place de l'upload du system de banque

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -20,6 +20,7 @@ class UploadController extends Controller
         return match ($request->type) {
             'engine' => $this->uploadEngine($request),
             'rental' => $this->uploadRental($request),
+            'banque' => $this->uploadBanque($request),
             default => abort(404, 'Type inconnu'),
         };
     }
@@ -27,6 +28,7 @@ class UploadController extends Controller
     private function uploadEngine(Request $request)
     {
         $request->file('file')->storeAs('uploads', $request->file('file')->getClientOriginalName());
+
         return redirect()->back();
     }
 
@@ -50,6 +52,32 @@ class UploadController extends Controller
             Storage::delete('logos/rentals/'.\Str::lower($rental->name).'.'.$request->file('file')->getClientOriginalExtension());
 
             toastr()->addSuccess("L'image a été créée");
+        } catch (Exception $exception) {
+            toastr()->addError($exception->getMessage());
+        }
+
+        return redirect()->back();
+    }
+
+    private function uploadBanque(Request $request)
+    {
+        $banque = RailwayBanque::find($request->get('model'));
+        try {
+            $request->file->storeAs(
+                'logos/banks',
+                \Str::lower($banque->name).'.'.$request->file('file')->extension(),
+            );
+
+            dispatch(new FormatImageJob(
+                \Storage::path('logos/banks/'.\Str::lower($banque->name).'.'.$request->file('file')->getClientOriginalExtension()),
+                directoryUpload: Storage::path('logos/banks'),
+                sector: 'banque',
+                nameFile: \Str::lower($banque->name),
+            ));
+
+            Storage::delete('logos/banks/'.\Str::lower($banque->name).'.'.$request->file('file')->getClientOriginalExtension());
+
+            toastr()->addSuccess("L'image a été crée");
         } catch (Exception $exception) {
             toastr()->addError($exception->getMessage());
         }
