90d40f623cc51c8558307ebbfc3605473dc38c73
Vortechstudio <contact@vortechstudio.fr>
2024-04-26 17:20:52 +0200

    feat(Railway Finance): Ajout sur la vue d'un service financier
    - Ajout d'information usuel sur les interets et type d'emprunt accessible sur un service financier.

--- resources/views/railway/finance/show.blade.php
+++ resources/views/railway/finance/show.blade.php
@@ -23,13 +23,31 @@
                     <div class="d-flex flex-row justify-content-between align-items-center">
                         <span class="fw-bold">Intêret</span>
                         <div class="d-flex flex-column">
-                            <div>
+                            <div class="d-flex justify-content-end">
                                 <span class="fw-bold me-2">Interet Min:</span>
-                                <span>{{ $banque->interest_min }}</span>
+                                <span>{{ number_format($banque->interest_min, 2, '.', ' ')." %" }}</span>
                             </div>
-                            <div>
+                            <div class="d-flex justify-content-end">
                                 <span class="fw-bold me-2">Interet Max:</span>
-                                <span>{{ $banque->interest_max }}</span>
+                                <span>{{ number_format($banque->interest_max, 2, '.', ' ')." %" }}</span>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <span class="fw-bold me-2">Interet Actuel:</span>
+                                <span class="badge badge-primary">{{ number_format($banque->latest_flux->interest, 2, '.', ' ')." %" }}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="separator separator-2 border-gray-300 my-3"></div>
+                    <div class="d-flex flex-row justify-content-between align-items-center">
+                        <span class="fw-bold">Emprunt disponible</span>
+                        <div class="d-flex flex-column">
+                            <div class="d-flex justify-content-end">
+                                <span class="fw-bold me-2">Emprunt express:</span>
+                                <span>{{ eur($banque->express_base) }}</span>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <span class="fw-bold me-2">Emprunt marché financier:</span>
+                                <span>{{ eur($banque->public_base) }}</span>
                             </div>
                         </div>
                     </div>
