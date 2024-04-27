a5f6e8b783aea87f59a4fa217b1c0c8f58d9b3a9
Vortechstudio <contact@vortechstudio.fr>
2024-04-27 20:32:23 +0200

    feat(Railway Bonus): Ajout visuel de type alerte pour alerter l'utilisateur de la réinitialisation des bonus dans X temps.

--- resources/views/railway/bonus/index.blade.php
+++ resources/views/railway/bonus/index.blade.php
@@ -10,6 +10,22 @@
 
     <div>
         <div class="rounded bg-white mb-10">
+            <div class="alert alert-dismissible bg-light-primary d-flex flex-column flex-sm-row w-100 p-5 mb-5">
+                <!--begin::Icon-->
+                <i class="fa-solid fa-clock fs-2hx text-primary me-4 mb-5 mb-sm-0"></i>                    <!--end::Icon-->
+
+                <!--begin::Content-->
+                <div class="d-flex flex-column pe-0 pe-sm-10">
+                    <h4 class="fw-semibold">Réinitialisation des bonus</h4>
+                    <span>Les bonus se réinitialiseront <span class="fw-bold" data-ago="{{ time() + now()->diffInSeconds(now()->endOfMonth()) }}"></span></span>
+                </div>
+                <!--end::Content-->
+
+                <!--begin::Close-->
+                <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
+                    <i class="ki-duotone ki-cross fs-1 text-primary"><span class="path1"></span><span class="path2"></span></i>                    </button>
+                <!--end::Close-->
+            </div>
             <div class="d-flex flex-wrap justify-content-center align-items-center gap-5 p-5">
                 @foreach($bonuses as $bonus)
                     <div class="card shadow-lg w-150px h-250px mb-10">
@@ -32,3 +48,94 @@
         </div>
     </div>
 @endsection
+
+@push("scripts")
+    <script type="text/javascript">
+        if (NodeList.prototype.forEach === undefined) {
+            NodeList.prototype.forEach = function (callback) {
+                [].forEach.call(this, callback)
+            }
+        }
+
+        let terms = [{
+            time: 45,
+            divide: 60,
+            text: 'moins d\'une minute'
+        }, {
+            time: 90,
+            divide: 60,
+            text: 'environ une minute'
+        }, {
+            time: 45 * 60,
+            divide: 60,
+            text: '%d minutes'
+        }, {
+            time: 90 * 60,
+            divide: 60 * 60,
+            text: 'environ une heure'
+        }, {
+            time: 24 * 60 * 60,
+            divide: 60 * 60,
+            text: '%d heures'
+        }, {
+            time: 42 * 60 * 60,
+            divide: 24 * 60 * 60,
+            text: 'environ un jour'
+        }, {
+            time: 30 * 24 * 60 * 60,
+            divide: 24 * 60 * 60,
+            text: '%d jours'
+        }, {
+            time: 45 * 24 * 60 * 60,
+            divide: 24 * 60 * 60 * 30,
+            text: 'environ un mois'
+        }, {
+            time: 365 * 24 * 60 * 60,
+            divide: 24 * 60 * 60 * 30,
+            text: '%d mois'
+        }, {
+            time: 365 * 1.5 * 24 * 60 * 60,
+            divide: 24 * 60 * 60 * 365,
+            text: 'environ un an'
+        }, {
+            time: Infinity,
+            divide: 24 * 60 * 60 * 365,
+            text: '%d ans'
+        }]
+
+        document.querySelectorAll('[data-ago]').forEach(function (node) {
+
+            let date = parseInt(node.dataset.ago, 10)
+
+            function setText () {
+                let secondes = Math.floor((new Date()).getTime() / 1000 - date)
+                let prefix = secondes > 0 ? 'Il y a ' : 'Dans '
+                let term = null
+                secondes = Math.abs(secondes)
+                for (term of terms) {
+                    if (secondes < term.time) {
+                        break
+                    }
+                }
+                node.innerHTML = prefix + term.text.replace('%d', Math.round(secondes / term.divide))
+
+                let nextTick = secondes % term.divide
+                if (nextTick === 0) {
+                    nextTick = term.divide
+                }
+
+                window.setTimeout(function () {
+                    if (node.parentNode) {
+                        if (window.requestAnimationFrame) {
+                            window.requestAnimationFrame(setText)
+                        } else {
+                            setText()
+                        }
+                    }
+                }, nextTick * 1000)
+            }
+
+            setText()
+        })
+    </script>
+@endpush
