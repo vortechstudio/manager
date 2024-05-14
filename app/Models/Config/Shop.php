2f87073938a7d968e3ef32a91962ef9e09223aba
Vortechstudio <contact@vortechstudio.fr>
2024-05-14 17:38:33 +0200

    feat(Boutique): Ajoute une relation entre les services et les boutiques
    - Ajoute une méthode shops() au modèle Service
    - Création d'une nouvelle table 'shops' via une migration
    - Ajoute un nouveau modèle Shop avec une relation vers Service

---
+++ app/Models/Config/Shop.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Config;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Shop extends Model
+{
+    public $timestamps = false;
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class, 'service_id');
+    }
+}
