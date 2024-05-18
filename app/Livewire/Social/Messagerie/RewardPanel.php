3fd4f9a23eb2f2c82ae1f072162664681e98e5a4
Vortechstudio <contact@vortechstudio.fr>
2024-05-18 04:10:56 +0200

    feat(Messagerie): Ajoute le composant RewardPanel
    - Ajout d'un nouveau fichier RewardPanel.php dans Livewire/Social/Messagerie
    - Création de la vue associée reward-panel.blade.php dans resources/views/livewire/social/messagerie

---
+++ app/Livewire/Social/Messagerie/RewardPanel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Social\Messagerie;
+
+use Livewire\Component;
+
+class RewardPanel extends Component
+{
+    public function render()
+    {
+        return view('livewire.social.messagerie.reward-panel');
+    }
+}
