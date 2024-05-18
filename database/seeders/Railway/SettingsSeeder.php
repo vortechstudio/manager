589bf02afecb2f28b39c20d3bbd0cfd03aa68e8d
Vortechstudio <contact@vortechstudio.fr>
2024-05-18 19:15:33 +0200

    refactor: mise à jour et simplification des modèles, migrations et seeders - Modification de la relation "services" dans le modèle User - Utilisation directe des queries au lieu des modèles dans SettingsSeeder - Ajustement de valeurs par défaut et suppression de cascades dans la migration create_user_services_table - Ajout d'une source .env et procédure de suppression/création de la base de données dans update-staging.sh - Ajout d'une procédure de suppression/création de base de données dans le Makefile

--- database/seeders/Railway/SettingsSeeder.php
+++ database/seeders/Railway/SettingsSeeder.php
@@ -10,56 +10,56 @@ class SettingsSeeder extends Seeder
     public function run(): void
     {
         if (! RailwaySetting::where('name', 'like', '%price_electricity%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'price_electricity',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%price_diesel%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'price_diesel',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%price_parking%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'price_parking',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%price_kilometer%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'price_kilometer',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%start_argent%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'start_argent',
                 'value' => 3000000,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%start_tpoint%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'start_tpoint',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%start_research%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'start_research',
                 'value' => 0,
             ]);
         }
 
         if (! RailwaySetting::where('name', 'like', '%exchange_tpoint%')->exists()) {
-            RailwaySetting::create([
+            \DB::connection('railway')->table('railway_settings')->insert([
                 'name' => 'exchange_tpoint',
                 'value' => 0,
             ]);
--- update-staging.sh
+++ update-staging.sh
@@ -1,4 +1,5 @@
 #\bin\bash
+source .env
 
 php artisan service:locked
 php artisan down
@@ -11,6 +12,14 @@ npm install
 
 if [ "$(git diff --name-only HEAD HEAD~1 -- database/migration)" != "" ]
 then
+    mysql -u votre_nom_utilisateur -p votre_mot_de_passe -e "
+
+    # Suppression de la base de données
+    DROP DATABASE IF EXISTS ${DB_RAILWAY_DATABASE};
+
+    # Création de la base de données
+    CREATE DATABASE ${DB_RAILWAY_DATABASE};
+    "
     php artisan migrate:fresh --seed
 fi
 
