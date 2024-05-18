4c724944742312a9c86e3f7ceb40bed47b1aaf92
Vortechstudio <contact@vortechstudio.fr>
2024-05-18 22:46:13 +0200

    feat(Railway Succès & Trophées): Ajout d'une page de gestion des succès et des trophées
    - Création de la vue index pour le module achievement
    - Ajout du script JavaScript dans la vue
    - Mise en place du toolbar dans la vue avec un titre et un breadcrumb appropriés

---
+++ resources/views/railway/achievement/index.blade.php
@@ -0,0 +1,17 @@
+@extends("layouts.app")
+@section("title")
+    Gestion des Succès & Trophées
+@endsection
+
+@section("content")
+    <x-base.toolbar
+        title="Gestion des Succès & Trophées"
+        :breads="array('Railway Manager', 'Gestion des Succès & Trophées')" />
+
+@endsection
+
+@push("scripts")
+    <script type="text/javascript">
+
+    </script>
+@endpush
