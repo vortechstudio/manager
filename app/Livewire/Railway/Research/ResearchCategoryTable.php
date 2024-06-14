f7c9dec1819b4928f7cb37f79735b16cb4ac5ad1
Vortechstudio <contact@vortechstudio.fr>
2024-06-14 21:01:45 +0200

    dix(Railway R&D): Correction de la logique d'import des recherches

--- app/Livewire/Railway/Research/ResearchCategoryTable.php
+++ app/Livewire/Railway/Research/ResearchCategoryTable.php
@@ -6,6 +6,8 @@ use App\Actions\ErrorDispatchHandle;
 use App\Models\Railway\Research\RailwayResearchCategory;
 use App\Models\Railway\Research\RailwayResearches;
 use App\Models\Railway\Research\RailwayResearchTrigger;
+use App\Models\User\Railway\UserRailway;
+use App\Models\User\ResearchUser;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
@@ -62,7 +64,7 @@ class ResearchCategoryTable extends Component
             }
 
             foreach ($researches as $research) {
-                RailwayResearches::updateOrCreate(['id' => $research['id']], [
+                $r = RailwayResearches::updateOrCreate(['id' => $research['id']], [
                     'id' => $research['id'],
                     'name' => $research['name'],
                     'description' => $research['description'],
@@ -72,6 +74,15 @@ class ResearchCategoryTable extends Component
                     'level_description' => $research['level_description'],
                     'railway_research_category_id' => $research['railway_research_category_id'],
                 ]);
+                foreach (UserRailway::all() as $user) {
+                    if (!ResearchUser::where('user_railway_id', $user->id)->where('railway_research_id', $r->id)->exists()) {
+                        ResearchUser::create([
+                            'user_railway_id' => $user->id,
+                            'railway_research_id' => $r->id,
+                            'is_unlocked' => !$r->parent_id,
+                        ]);
+                    }
+                }
             }
 
             foreach ($triggers as $trigger) {
